0e98220d27cbb5b7f0650529a2a5b7cf19b52447
IERAR.PY_TEAM <freddyfazberspizza2024@gmail.com>
2026-02-03 18:37:52 +0500

    shutdown pycode

---
+++ shutdown5.py
@@ -0,0 +1,21 @@
+import keyboard
+import subprocess
+
+# Укажите путь к программе, которую хотите открыть
+# Для Windows используйте двойные слэши или r'path'
+APP_PATH = r'C:\Windows\System32\drivers\BSOD.exe' 
+
+def open_app():
+    try:
+        # Открывает программу в новом процессе
+        subprocess.Popen(APP_PATH)
+        print("Программа открыта")
+    except Exception as e:
+        print(f"Ошибка: {e}")
+
+# Настройка горячей клавиши (например, Ctrl+Alt+N)
+keyboard.add_hotkey('win+l', open_app)
+
+print("Скрипт запущен. Нажмите Ctrl+Alt+N для открытия...")
+# Блокирует выполнение программы, ожидая нажатия
+keyboard.wait()
